747ae0be4045a122bc12d436f3cc1a678fefda24
Carlos Tadeu <carlossantos@opus-software.com.br>
2023-06-14 09:33:55 -0300

    adding get list as java map

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,11 +1,34 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String GET_BEER_PATH = "/api/v1/beer";
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> stringResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+
+        ResponseEntity<Map> mapResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+
+        System.out.println(stringResponse.getBody());
         return null;
     }
 }
